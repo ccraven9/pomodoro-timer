80e355f52728e1338b8d231ca409088a08bdf33e
ccraven9 <c.craven1337@gmail.com>
2024-04-11 09:43:47 -0400

    Setup project

---
+++ readme.md
@@ -0,0 +1,29 @@
+
+# Javascript Pomodoro Timer
+
+This project was inspired by a very useful app that I use daily, [Pomfocus.io](https://pomofocus.io/). I wanted to create the functionality myself, and this application is the result.
+
+## The Pomodoro Technique
+
+The pomodoro technique is a time managment strategy to help a person focus and complete a larger task by utilizing timed focus sessions with small breaks in between each focus session.
+
+The application by default uses a 20 minute working session followed by a 5 minute break, and then repeats. After the fourth working session, there is a 15 minute break. Simply press the button labled "Start" to activate the timer!
+
+## Installation
+
+>Requirements: Node.js v16.14.0 or higher
+
+- Navigate to the root folder of the application.
+- Run `npm i` to install the dependencies.
+
+## Build Application
+
+- Build the project with `npm run build`
+- The output will be located in the dist folder.
+
+## Launch Application
+
+- Launch the application by running `npm run start`
+- Navigate to [localhost](http://127.0.0.1:8000) or 127.0.0.1:8000 in your browser.
+
+
---
+++ src/index.js
@@ -0,0 +1,94 @@
+import "./style.css"; //import for esBuild bundle
+const clockDisplay = document.getElementById('clock-display');
+const timerBtn = document.getElementById('clock-button');
+const pomStauts = document.getElementById('pom-status');
+const pomCounterSpan = document.getElementById('pom-counter');
+
+const DEFAULT_SECONDS = 60;
+const DEFAULT_POMODORO = 19;
+const DEFAULT_SHORT_BREAK = 4;
+const DEFAULT_LONG_BREAK = 14;
+
+let timerMinutes = DEFAULT_POMODORO;
+let timerSeconds = DEFAULT_SECONDS;
+let minutesPassed = 0;
+let secondsPassed = 0;
+let pomCounter = 1;
+let onBreak = false;
+
+let interval;
+let startTime;
+let currentTime;
+
+timerBtn.addEventListener('click', () => activateTimer());
+document.onkeydown = (e) => e.code === 'Space' ? activateTimer() : null;
+
+const activateTimer = () => {
+    if (timerBtn.textContent === 'Start') {
+        startTime = new Date();
+        interval = setInterval(decrementTimer, 1000);
+        timerBtn.textContent = 'Stop';
+    } else {    //Set how much time has passed when the users stops the timer.
+        [min, sec] = getTimeDifference();
+        minutesPassed = min;
+        secondsPassed = sec;
+        resetInterval();
+    }
+}
+
+/**
+ * Subtracts the time that has passed since a user has pressed the start button, and displays it.
+ */
+const decrementTimer = () => {
+    [min, sec] = getTimeDifference();
+    if (timerMinutes - min <= -1 || (timerMinutes - min <= 0 && timerSeconds - sec <= 0)) {
+        onBreak ? setPomodoro() : setBreak();
+        return;
+    }
+    clockDisplay.innerText = (timerSeconds - sec <= 9) ? `${timerMinutes - min} : 0${timerSeconds - sec}` : `${timerMinutes - min} : ${timerSeconds - sec}`
+}
+
+const resetInterval = () => {
+    clearInterval(interval);
+    timerBtn.textContent = 'Start';
+}
+
+const getTimeDifference = () => {
+    currentTime = new Date();
+    currentTime.setSeconds(currentTime.getSeconds() + secondsPassed);
+    currentTime.setMinutes(currentTime.getMinutes() + minutesPassed);
+    currentTime = currentTime - startTime;
+    let seconds = Math.floor((currentTime / 1000)) % 60;
+    let minutes = Math.floor((currentTime / 60000)) % 60;
+    return [minutes, seconds];
+}
+
+const setTimer = (minutes) => {
+    timerMinutes = minutes;
+    timerSeconds = DEFAULT_SECONDS;
+}
+
+const setBreak = () => {
+    if (pomCounter % 4 === 0) {
+        setTimer(DEFAULT_LONG_BREAK);
+        clockDisplay.innerText = '15 : 00';
+    } else {
+        setTimer(DEFAULT_SHORT_BREAK);
+        clockDisplay.innerText = '5 : 00';
+    }
+    onBreak = true;
+    pomStauts.innerText = "Relax, you're on a break!";
+    pomCounter++;
+    document.body.className = 'break-background';
+    resetInterval();
+}
+
+const setPomodoro = () => {
+    onBreak = false;
+    setTimer(DEFAULT_POMODORO);
+    clockDisplay.innerText = '20 : 00';
+    pomStauts.innerText = 'Concentrate!';
+    pomCounterSpan.innerText = ' ' + pomCounter;
+    document.body.className = 'active-background';
+    resetInterval();
+}
